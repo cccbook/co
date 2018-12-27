1c86da267d19d5e2ec1b5e2dfcb6f53cac2cf74e
ccckmit <ccckmit@gmail.com>
2018-12-27 12:28:36 +0800

    simplify  mux

--- code/verilog/nand2tetris/mux_test.v
+++ code/verilog/nand2tetris/mux_test.v
@@ -7,14 +7,14 @@ wire[15:0] mux2, mux4, mux8;
 wire mux01, dmux0, dmux1;
 
 Mux       g1(1'b0, 1'b1, sel[2], mux01);
-DMux      g2(a[0], sel[2], dmux0, dmux1);
+DMux      g2(1'b1, sel[0], dmux0, dmux1);
 Mux16     g4(a, b, sel[0], mux2);
 Mux4Way16 g5(a, b, c, d, sel[1:0], mux4);
 Mux8Way16 g6(a, b, c, d, e, f, g, h, sel[2:0], mux8);
 
 initial
 begin
-  $monitor("%4dns sel=%d mux2=%x mux4=%x mux8=%x", $stime, sel, mux2, mux4, mux8);
+  $monitor("%4dns sel=%d mux2=%x mux4=%x mux8=%x mux01=%d dmux0=%d dmux1=%d", $stime, sel, mux2, mux4, mux8, mux01, dmux0, dmux1);
   a  = 16'h0;
   b  = 16'h1;
   c  = 16'h2;
